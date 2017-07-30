b2a1f8d7a3d2b2d562c5adb396859e2f8ea53c8f
mrchan <wildcatrobotics2011@gmail.com>
2017-07-29 22:35:20 -0700

    cascade effect

---
+++ fireworks.js
@@ -0,0 +1,34 @@
+storage = {'body': $('body'), 'colorDeg': 0};
+
+var pickRandomDir = function(){
+  return Math.floor(Math.random()*360);
+}
+
+var getColor = function(){
+  var ret = 'rgb(150,'+Math.floor(Math.sin(storage.colorDeg)*255)+','+Math.floor(Math.cos(storage.colorDeg)*255)+')';
+  storage.colorDeg+=0.04
+  console.log(ret)
+  return ret;
+}
+
+var createThing = function(){
+  if(!(storage.x&&storage.y))return;
+  var dot = $('<div/>')
+    .addClass('dot')
+    .css('top',storage.y)
+    .css('left',storage.x)
+    .css('background-color',getColor());
+  storage.body.append(dot);
+  dot.animate({'top':storage.y+200,'left':storage.x-10,'width':20,'height':20,'opacity':0}, 1000);
+  setTimeout(function(){
+    dot.remove();
+  },1000);
+}
+
+var moveDiv = function(event){
+  storage.x = event.pageX;
+  storage.y = event.pageY;
+}
+
+storage.body.mousemove(moveDiv)
+setInterval(createThing, 20);
