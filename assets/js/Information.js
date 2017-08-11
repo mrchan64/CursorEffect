d295602355a9178101f47da0a2b478fda47c1c1d
Matt Chan <wildcatrobotics2011@gmail.com>
2017-08-10 23:40:03 -0700

    made icons

--- assets/js/Information.js
+++ assets/js/Information.js
@@ -5,7 +5,7 @@ function InfoPanel() {
     var porps = this.porps = body.node().getBoundingClientRect();
     var viewport = this.viewport = this.body.append('container')
       .attr('id', 'info-container');
-    this.boxWidth = 1-mrchan.config.directory.boxWidth*.8;
+    this.boxWidth = (1-mrchan.config.directory.boxWidth)*.8;
     this.infoList = [];
     this.addInfo();
     this.addInfo();
@@ -21,7 +21,7 @@ function InfoPanel() {
     var dims = mrchan.storage.NamePlate.namePlate.node().getBoundingClientRect();
     var minLeft = (this.porps.width-dims.width)/2*.3+dims.width;
     this.viewport
-      .style('width', porps.width*(1-this.boxWidth) < minLeft ? porps.width-minLeft : porps.width*this.boxWidth+'px')
+      .style('width', porps.width*(1-this.boxWidth) < minLeft ? porps.width-minLeft+'px' : porps.width*this.boxWidth+'px')
       .style('height', porps.height*.7+'px')
       .style('top', porps.height*.15+'px');
     this.viewport.selectAll(".info-container-item")
